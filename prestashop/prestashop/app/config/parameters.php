18e0249320b2e518c34bf28273b5dd241aca4c9d
Jon Echebarria <jechebarria008@ikasle.ehu.eus>
2017-10-26 00:02:39 +0200

    Añadido contenido web de prestashop

---
+++ prestashop/prestashop/app/config/parameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => '127.0.0.1',
+    'database_port' => '',
+    'database_name' => 'prestaShop',
+    'database_user' => 'vortex',
+    'database_password' => '1234',
+    'database_prefix' => 'prestashop_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'fkIzFY4jz4PIbCN08UK9aHipKP4RKX6qQSsHVtwGHm2E7xDSQOPSxIko',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2017-10-14',
+    'locale' => 'en-US',
+    'cookie_key' => 'ZkdF1VVzCQEJGDuW6Jneadw5k5WYjjevYEvmHK4ZVJqnywoQvquz7tBN',
+    'cookie_iv' => 'HcxXAGNM',
+    'new_cookie_key' => 'def00000dc5b87dc0f889955415e331752b9a750f4f54c6cfcf45e0474adae25d331d62d2d897b57265401777b2798de309dc682ebdb81fb5314070e28ae509101417233',
+  ),
+);
