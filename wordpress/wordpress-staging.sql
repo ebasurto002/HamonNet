e5885920251dd2a63e4de2d797cdcbc2e641e466
Jon Echebarria <jechebarria008@ikasle.ehu.eus>
2017-11-01 14:50:19 +0100

    Añadida configuración sql de staging para wordpress

---
+++ wordpress/wordpress-staging.sql
@@ -0,0 +1,2 @@
+UPDATE `prestaShop`.`wp_options` SET `option_value`='http://www-staging.blackartdsgns.com' WHERE `option_id`='1';
+UPDATE `prestaShop`.`wp_options` SET `option_value`='http://www-staging.blackartdsgns.com' WHERE `option_id`='2'; 
