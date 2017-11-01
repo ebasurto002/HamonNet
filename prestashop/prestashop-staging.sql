0944e6329935495ab5a139d8acf4a42ef566d98b
Jon Echebarria <jechebarria008@ikasle.ehu.eus>
2017-11-01 14:35:56 +0100

    Añadida configuración sql para prestashop staging

---
+++ prestashop/prestashop-staging.sql
@@ -0,0 +1,3 @@
+UPDATE `prestaShop`.`ps_configuration` SET `value`='prestashop-staging.blackartdsgns.com' WHERE `id_configuration`='229';
+UPDATE `prestaShop`.`ps_configuration` SET `value`='prestashop-staging.blackartdsgns.com' WHERE `id_configuration`='230';
+UPDATE `prestaShop`.`ps_shop_url` SET `domain`='prestashop-staging.blackartdsgns.com', `domain_ssl`='prestashop-staging.blackartdsgns.com' WHERE `id_shop_url`='1'; 
